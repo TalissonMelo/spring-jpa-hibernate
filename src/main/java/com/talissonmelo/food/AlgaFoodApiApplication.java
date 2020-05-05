96b93b02ab77269a1ecc8663614e846dd4748444
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-05-05 10:22:39 -0300

    Adicionando JPA e configurando o Data Source

---
+++ src/main/java/com/talissonmelo/food/AlgaFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talissonmelo/food/AlgaFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
