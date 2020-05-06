78cab9cbd89bd28267abec6db074ec58fe56a69c
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-05-06 10:10:43 -0300

    Atualizando um objeto no banco de dados

---
+++ src/main/java/com/talissonmelo/food/jpa/KitchenUpdateMain.java
@@ -0,0 +1,27 @@
+package com.talissonmelo.food.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.talissonmelo.food.AlgaFoodApiApplication;
+import com.talissonmelo.food.domain.model.Kitchen;
+
+public class KitchenUpdateMain {
+
+	public static void main(String[] args) {
+
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgaFoodApiApplication.class)
+				.web(WebApplicationType.NONE).run(args);
+
+		KitchenJPA jpa = applicationContext.getBean(KitchenJPA.class);
+		
+		Kitchen kitchen = new Kitchen();
+		kitchen.setId(1L);
+		kitchen.setName("Brasileira");
+		
+		jpa.add(kitchen);
+
+	}
+
+}
