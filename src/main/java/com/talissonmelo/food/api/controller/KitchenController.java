de1368f98e8dc62bfbd7be24d9c1122ef2e9a43f
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-05-07 08:50:15 -0300

    Modelando e requisitando um Collection Resource com GET

---
+++ src/main/java/com/talissonmelo/food/api/controller/KitchenController.java
@@ -0,0 +1,25 @@
+package com.talissonmelo.food.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talissonmelo.food.domain.model.Kitchen;
+import com.talissonmelo.food.domain.model.repository.KitchenRepository;
+
+@RestController
+@RequestMapping(value = "/kitchen")
+public class KitchenController {
+	
+	@Autowired
+	private KitchenRepository repository;
+	
+	@GetMapping
+	public List<Kitchen> findAll(){
+		return repository.findAll();
+	}
+
+}
